091bb96cac5bb7ec6cb610b41759e68826470851
Gorm Casper <g@g.dk>
2016-03-15 13:57:38 +0100

    Add some more methods

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 var botkit = require('botkit');
+var EventEmitter = require('events').EventEmitter;
+var TESTER_NAME = 'robbie';
 
 module.exports = function (opts) {
-  var that = {};
+  var that = new EventEmitter();
   var controller = botkit.slackbot();
   var currentChannel;
   var channels = {};
   var isRTMOpen = false;
   var rtmOpenCbs = [];
   var replyCbs = [];
+  var mentionCbs = [];
+  var matchCbs = [];
+  var bot;
 
-  var bot = controller.spawn({
+  var tester = controller.spawn({
     token: opts.token,
   }).startRTM();
 
-  controller.hears('', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-    if (!replyCbs.length) return;
-    replyCbs.shift()(null, message);
+  tester.api.users.list({}, function (err, users) {
+    if (err) return that.emit('error', err);
+    bot = users.members.filter(function (user) {
+      return user.name === opts.name;
+    })[0];
+    if (!bot) that.emit('error', new Error('Couldn\'t find the bot'));
+  })
+
+  controller.hears('', ['message', 'direct_message', 'mention', 'direct_mention'], function(tester, message) {
+    if (replyCbs.length) replyCbs.shift()(null, message);
+    if (mentionCbs.length && message.event === 'direct_mention') mentionCbs.shift()(null, message);
+    if (!matchCbs.length) return;
+    var idx = matchCbs.reduce(function (idx, matchCb, i) {
+      return idx === -1 && message.text.match(matchCb.regex) ? i : idx;
+    }, -1);
+    if (idx === -1) return;
+    matchCbs[idx].cb(message);
+    matchCbs = matchCbs.slice(0, idx).concat(matchCbs.slice(idx + 1)); // Remove the matching cb
   });
 
   controller.on('rtm_open', function () {
@@ -28,7 +48,7 @@ module.exports = function (opts) {
 
   that.join = function (channel, cb) {
     cb = cb || function () {};
-    bot.api.channels.list({}, onlist);
+    tester.api.channels.list({}, onlist);
 
     function onlist (err, chs) {
       if (err) return cb(err);
@@ -50,11 +70,31 @@ module.exports = function (opts) {
     var ch = currentChannel;
     if (opts.channel) ch = channels[opts.channel];
 
-    bot.say({text: message, channel: ch});
+    tester.say({text: message, channel: ch});
   };
+
+  that.mention = function (message, opts) {
+    that.say('@'+bot.name+': ' + message, opts);
+  };
+
+  that.dm = function (message, opts) {
+    if (!isRTMOpen) return rtmOpenCbs.push(that.dm.bind(null, message, opts));
+
+    tester.startPrivateConversation({user: bot.id}, function (response, convo){
+      convo.say(message);
+    });
+  }
 
   that.nextReply = function (cb) {
     replyCbs.push(cb);
+  };
+
+  that.nextMention = function (cb) {
+    mentionCbs.push(cb);
+  };
+
+  that.nextMatch = function (regex, cb) {
+    matchCbs.push({regex: regex, cb: cb});
   };
 
   return that;
